928005daee72d836232aa73d42972c616a2e4c6f
Volodymyr <>
2024-08-03 09:25:22 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+How to run the tests:
+
+```
+nvm use
+npm install
+npm run wdio
+```
---
+++ test.e2e.js
@@ -0,0 +1,37 @@
+import { expect, browser } from "@wdio/globals";
+
+browser.addCommand(
+  "myCommand",
+  () => {
+    return 1;
+  },
+  true
+);
+
+describe("Custom command test", () => {
+  const existingSel = "#username";
+  const nonExistingSel = "#username123";
+
+  beforeEach(async () => {
+    await browser.url("https://the-internet.herokuapp.com/login");
+    await expect($(existingSel)).toBeExisting();
+    await expect($(nonExistingSel)).not.toBeExisting();
+  });
+
+  it("default isDisplayed() should work", async () => {
+    const resultExistingSel = await $(existingSel).isDisplayed();
+    expect(resultExistingSel).toBe(true);
+
+    const resultNonExistingSel = await $(nonExistingSel).isDisplayed();
+    expect(resultNonExistingSel).toBe(false);
+  });
+
+  it("custom myCommand() should work", async () => {
+    const resultExistingSel = await $(existingSel).myCommand();
+    expect(resultExistingSel).toBe(1);
+
+    // ERROR: Can't call customCommand on element with selector "#username123" because element wasn't found
+    const resultNonExistingSel = await $(nonExistingSel).myCommand();
+    expect(resultNonExistingSel).toBe(1);
+  });
+});
